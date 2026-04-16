69e58f76f5f50cf6dca9b8028dd36295fa9984f6
Liander <lian10der@gmail.com>
2026-04-16 16:05:15 -0300

    fix: animação 3d consertada

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
 const qrCodeButton = document.getElementById("qrCode")
 const qrImagem = document.querySelector(".qrcode")
 
-qrCodeButton.addEventListener("click", function (){
-    console.log(qrImagem)
-    qrImagem.classList.add('girar');
 
-    setTimeout(function(){
-        qrImagem.classList.remove('girar');
-    }, 1000)
+qrImagem.addEventListener("click", () => {
+    qrImagem.classList.toggle('girar');
 })
 
